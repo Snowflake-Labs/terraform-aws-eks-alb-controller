9150983c0bcdcf9c6aa0134558827c9e7f04f677
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-02-01 08:45:32 -0800

    Add helm provider.

--- provider.tf
+++ provider.tf
@@ -9,3 +9,16 @@ provider "kubernetes" {
   }
   load_config_file = false
 }
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.pat_eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.pat_eks_cluster.certificate_authority.0.data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.pat_eks_cluster.name]
+      command     = "aws"
+    }
+  }
+}
