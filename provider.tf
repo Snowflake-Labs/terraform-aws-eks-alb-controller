fd27f68b261e326b373c8a65a3da213e151fed84
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-02-01 08:46:21 -0800

    Add helm provider.

--- provider.tf
+++ provider.tf
@@ -12,12 +12,12 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.pat_eks_cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.pat_eks_cluster.certificate_authority.0.data)
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 
     exec {
       api_version = "client.authentication.k8s.io/v1alpha1"
-      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.pat_eks_cluster.name]
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
       command     = "aws"
     }
   }
