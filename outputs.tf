49a6ee76b58c57478fa42b473bf2cd2c97c38f20
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-02-01 14:50:33 -0800

    Outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,48 @@
 output "eks_cluster_id" {
   value = module.eks.cluster_id
 }
+
+output "eks_cluster_id" {
+  value = module.eks.eks_cluster_id
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane."
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_primary_security_group_id" {
+  description = "Cluster security group that was created by Amazon EKS for the cluster. Managed node groups use this security group for control-plane-to-data-plane communication. Referred to as 'Cluster security group' in the EKS console."
+  value       = module.eks.cluster_primary_security_group_id
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ids attached to the cluster control plane."
+  value       = module.eks.cluster_security_group_id
+}
+
+output "node_security_group_id" {
+  description = "Security group ids attached to the worker nodes of the cluster."
+  value       = module.eks.cluster_security_group_id
+}
+
+output "cluster_iam_role_name" {
+  description = "Cluster IAM Role."
+  value       = module.eks.cluster_iam_role_name
+}
+
+output "cluster_iam_role_arn" {
+  description = "IAM role ARN of the EKS cluster."
+  value       = module.eks.cluster_iam_role_name
+}
+
+output "aws_auth_configmap_yaml" {
+  description = "A kubernetes configuration to authenticate to this EKS cluster."
+  value       = module.eks.aws_auth_configmap_yaml
+}
+
+output "eks_managed_node_groups" {
+  description = "Outputs from node groups"
+  value       = module.eks.eks_managed_node_groups
+}
+
