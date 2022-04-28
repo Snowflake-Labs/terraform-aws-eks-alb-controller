2dff8f77b6656515d3049cbbc42e01bce51cd272
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-04-28 09:49:51 -0700

    Fix alb controller version for 1.22.

--- helm_alb_controller.tf
+++ helm_alb_controller.tf
@@ -2,7 +2,7 @@ resource "helm_release" "lb_controller" {
   name       = "aws-load-balancer-controller"
   chart      = "aws-load-balancer-controller"
   repository = "https://aws.github.io/eks-charts"
-  version    = "1.3.3"
+  version    = "2.4.1"
 
   namespace = "kube-system"
   wait      = false
