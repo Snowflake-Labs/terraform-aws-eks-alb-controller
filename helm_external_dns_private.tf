4658b2d48bba45bc1c3b37244515ac10a46bae6c
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-04-28 13:58:55 -0700

    Fix IAM roles, K8s service accounts.

--- helm_external_dns_private.tf
+++ helm_external_dns_private.tf
@@ -12,7 +12,6 @@ resource "helm_release" "external_dns_private" {
       "${path.module}/templates/external_dns_private_values.yaml",
       {
         aws_region                       = "${var.aws_region}",
-        aws_zone_type                    = "private",
         aws_private_hosted_zone          = "${var.aws_private_hosted_zone}",
         external_dns_eks_service_account = "${aws_iam_role.external_dns_private_role.name}",
         aws_iam_role_external_dns        = "${aws_iam_role.external_dns_private_role.name}",
