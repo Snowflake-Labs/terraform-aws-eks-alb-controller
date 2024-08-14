b14aa37a88bed57b882094308b85329a6660b923
Vijaya Sai Prasanth Kommini <72515998+sfc-gh-pkommini@users.noreply.github.com>
2024-08-14 04:19:24 -0700

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "~> 1.4.6"
+  required_version = ">= 1.4.6"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.38.0"
-    }
+    aws        = ">= 4.38.0"
+    local      = ">= 2.2.3"
+    random     = ">= 3.4.3"
+    kubernetes = ">= 2.15.0"
   }
 }
