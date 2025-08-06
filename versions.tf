e26abb0d4c0fa8bd06982d9a5e076750f59917b9
Edwin Sam Sunder Bakthanathan <edwin.samsunderb@snowflake.com>
2025-08-06 09:04:04 +0530

    aws provider upgrade

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.4.6"
 
   required_providers {
-    aws        = ">= 4.38.0"
+    aws        = "~> 5.99.0"
     local      = ">= 2.2.3"
     random     = ">= 3.4.3"
     kubernetes = ">= 2.15.0"
