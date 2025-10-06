bbcfc3bbb3eb193f180726a683963fcddf1d867a
Bora Kou <bora.kou@snowflake.com>
2025-10-06 19:29:04 -0400

    Add vpc_cni_driver, coredns_driver, kube_proxy_driver addons.

--- addon_eks_pod_identity_agent.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_eks_addon" "addon_eks_pod_identity_agent" {
-  count = length(var.addon_eks_pod_identity_agent) == 0 ? 0 : 1
-
-  cluster_name      = module.eks.cluster_id
-  addon_name        = var.addon_eks_pod_identity_agent["addon_name"]
-  addon_version     = lookup(var.addon_eks_pod_identity_agent, "addon_version", null)
-  resolve_conflicts = lookup(var.addon_eks_pod_identity_agent, "resolve_conflicts", null)
-}
